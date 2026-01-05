fccf7cd82500eabe7d44b233f14cde9442c70890
Thibault Pouch <thibault.pouch@epitech.eu>
2026-01-05 09:37:45 +0100

    refactor: Integrate SonarQube configuration into build.gradle.kts and remove from settings.gradle.kts

--- mobile/android/build.gradle.kts
+++ mobile/android/build.gradle.kts
@@ -1,7 +1,18 @@
+plugins {
+    id("org.sonarqube") version "7.0.1.6134"
+}
+
 allprojects {
     repositories {
         google()
         mavenCentral()
+    }
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "Aincrad-Flux_AREA-Mobile_a69c10cd-189c-430e-b4a7-4719b173cf11")
+        property("sonar.projectName", "AREA-Mobile")
     }
 }
 
